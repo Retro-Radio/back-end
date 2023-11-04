89cb234dcfa5b349f31909257f7ff64b442ceddb
msternia <michal123sternal@gmail.com>
2023-11-04 20:19:36 +0000

    Convert files

--- src/audioProcessor.js
+++ src/audioProcessor.js
@@ -1,33 +1,50 @@
-const ffmpeg = require('ffmpeg');
-audio = require('fluent-ffmpeg/lib/options/audio');
 
-function audioProcessor(path) {
-    try {
 
-        console.log(path)
+function generateStream(inputAudioFile) {
+    const { exec } = require('child_process');
+    const path = require('path');
 
-        new ffmpeg(path, function (err, audio) {
-            if (!err) {
-                console.log(audio.metadata);
-                audio.save('../bin/audio.m3u8')
-                
-                
-            } else {
-                console.log('Error: ' + err);
-            }
-        });
+    inputDirectory = '../bin/download/';
+    const outputDirectory = '../bin/upload/';
 
-    } catch (e) {
-        console.log(e.code);
-        console.log(e.msg);
-
-        
+    const fs = require('fs');
+    if (!fs.existsSync(outputDirectory)) {
+    fs.mkdirSync(outputDirectory, { recursive: true });
     }
 
+    const ffmpegProbeCommand = `ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 ${path.join(inputDirectory, inputAudioFile)}`;
+
+    exec(ffmpegProbeCommand, (error, stdout) => {
+    if (error) {
+        console.error(`Error: ${error.message}`);
+        return;
+    }
+
+    const totalDuration = parseFloat(stdout); // Duration of audio file (in seconds)
+    
+    const segmentDuration = 2; // Duration of each segment (in seconds)
+    const numSegments = Math.ceil(totalDuration / segmentDuration); // Total number of segments
+
+    for (let i = 0; i < numSegments; i++) {
+        const startTime = i * segmentDuration;
+        const outputFileName = path.join(outputDirectory, `${inputAudioFile.substring(0, inputAudioFile.length - 4)}_${i.toString().padStart(3, '0')}.flac`);
+        console.log(outputFileName);
+        const generateFlacSegment = `ffmpeg -i ${path.join(inputDirectory, inputAudioFile)} -ss ${startTime} -t ${segmentDuration} -c:a flac -y "${outputFileName}"`;
+        
+        exec(generateFlacSegment, (ffmpegError, ffmpegStdout, ffmpegStderr) => {
+        if (ffmpegError) {
+            console.error(`Error: ${ffmpegError.message}`);
+            return;
+        }
+        if (ffmpegStderr) {
+            console.error(`FFmpeg Error: ${ffmpegStderr}`);
+        }
+        console.log(`Segment ${i + 1} completed.`);
+        });
+    }
+    });
+
+    return 1;
 }
 
-audioProcessor('ballin.mp3');
-
-
-//ffmpeg -i input.avi -c:v libx264 -crf 23 -c:a aac -movflags faststart output.mp4
-//ffmpeg -i 1.mp3 -map 0:a -c:a aac -b:a 320k -f hls -hls_time 10 -hls_list_size 0 -hls_segment_filename audio%04d.ts audio.m3u8
+generateStream('ballin.mp3');
