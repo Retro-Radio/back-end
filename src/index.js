692fb61ae76f6369c73eefeaf50ea26b33e2a5d8
umarf786 <umarf786_@outlook.com>
2023-11-05 03:58:13 +0000

    ballin

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
 const express = require('express');
-
+const cors = require('cors');
+const youtube = require("./youtubeFunctions.js");
+const fs = require('fs');
 const app = express();
 
+app.use(cors({origin: `*`}));
 app.use(express.json());
 
 PORT= 3000;
-
 app.listen(PORT, () => {
     console.log("Server Listening on PORT:", PORT);
+});
+
+app.post('/youtube', async (req, res) => {
+    let link  = req.body.link;
+    const parsedUrl = new URL(link);
+    const searchParams = new URLSearchParams(parsedUrl.search);
+    const videoId = searchParams.get("v");
+    console.log(link);
+    let title = await youtube.getTitle(link);
+    if (title == null) {
+        res.status(400).json({error: "Invalid Link"});
+        return;
+    }
+    path = await youtube.downloadVideo(link);
+    console.log("Path: " + path);
+    
+    res.status(200).json({title: title});
+  })
+
+  app.get('/audio-stream', async (req, res) => {
+    let { url } = req.query;
+    let videoId = url;
+    url += ".mp3";
+    console.log(url);
+    stat = fs.statSync(url);
+    res.writeHead(200, {
+        'Content-Type': 'audio/mpeg',
+        'Content-Length': stat.size,
+    });
+
+    fs.createReadStream(url).pipe(res);
   });
 
-app.post('/youtube', (req, res) => {
-    let link  = req.body.link;
-    res.status(200).send('Recieved the youtube link, starting to process');
-    console.log(link);
-  })
---
+++ src/youtubeFunctions.js
@@ -0,0 +1,38 @@
+const ytdl = require('ytdl-core');
+
+const getTitle = async (url) => {
+  try {
+    const info = await ytdl.getInfo(url);
+    const title = info.videoDetails.title;
+    console.log(title);
+    return title;
+  } catch (error) {
+    console.error('Error:', error);
+    return null;
+  }
+};
+
+const downloadVideo = async (url) => {
+    try {
+      title = await getTitle(url);
+      console.log(`Downloading video: ${title}`);
+      const parsedUrl = new URL(url);
+      const searchParams = new URLSearchParams(parsedUrl.search);
+      const videoId = searchParams.get("v");
+      return new Promise((resolve, reject) => {
+        const stream = ytdl(url, { quality: 'highest', filter: 'audioonly'});
+        const outputFileName = `${videoId}.mp3`;
+
+        stream.pipe(require('fs').createWriteStream(outputFileName));
+    
+        stream.on('end', () => {
+          console.log(`Video downloaded as ${outputFileName}`);
+          resolve(outputFileName);
+        })
+      });
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+module.exports = { downloadVideo, getTitle };
